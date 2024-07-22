0dcda2f7caa6794cd6369fe804a33dce4d7d2f63
shirish umredkar <shirish.gcp@gmail.com>
2024-07-22 18:58:15 +0530

    Latest Changes

--- archtechui/archtechuiangular/src/environments/environment.prod.ts
+++ archtechui/archtechuiangular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://34.44.131.21:80/archtechlogin'
+    //apiUrl: 'http://34.44.131.21:80/archtechlogin'
+    apiUrl: 'http://localhost:8090/archtechlogin'
   };
--- archtechui/archtechuiangular/src/environments/environment.ts
+++ archtechui/archtechuiangular/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://34.44.131.21:80/archtechlogin'
+    //apiUrl: 'http://34.44.131.21:80/archtechlogin'
+    apiUrl: 'http://localhost:8090/archtechlogin'
   };
